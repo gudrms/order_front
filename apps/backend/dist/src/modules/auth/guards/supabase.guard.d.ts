7436bd8133d4d4a51822db77bfe468a53d9794a3
Ji Hyung Geun <vndanwl@naver.com>
2025-12-30 15:55:50 +0900

    dist 제외

--- apps/backend/dist/src/modules/auth/guards/supabase.guard.d.ts
+++
@@ -1,4 +0,0 @@
-declare const SupabaseGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class SupabaseGuard extends SupabaseGuard_base {
-}
-export {};
