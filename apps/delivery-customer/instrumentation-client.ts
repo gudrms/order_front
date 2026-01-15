64d75b6fddf837b6e8150ba1f2a9a4b1c41ec4b2
Ji Hyung Geun <vndanwl@naver.com>
2026-01-15 16:14:18 +0900

    Sentry 테스트 페이지 및 에러 처리 엔드포인트 추가: 클라이언트/백엔드 에러 시뮬레이션 구현

--- apps/delivery-customer/instrumentation-client.ts
+++ apps/delivery-customer/instrumentation-client.ts
@@ -2,3 +2,7 @@ export async function register() {
   // Client-side Sentry 초기화
   await import('./sentry.client.config');
 }
+
+import * as Sentry from '@sentry/nextjs';
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
