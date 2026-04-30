49f4e9a0ef4ff7b7bf2eaa90784466e7366d7904
Ji Hyung Geun <vndanwl@naver.com>
2026-05-01 05:59:25 +0900

    백엔드 MQ 처리 엔드포인트 추가

--- apps/backend/src/modules/queue/queue.module.ts
+++ apps/backend/src/modules/queue/queue.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { QueueController } from './queue.controller';
 import { QueueConsumerService } from './queue-consumer.service';
 import { QueueService } from './queue.service';
 
 @Module({
     imports: [PrismaModule],
+    controllers: [QueueController],
     providers: [QueueService, QueueConsumerService],
     exports: [QueueService, QueueConsumerService],
 })
