c230e0846d436c547858ff4bea5a83e7b74ecf73
Ji Hyung Geun <vndanwl@naver.com>
2026-05-01 05:50:35 +0900

    백엔드 MQ 최소 소비자 추가

--- apps/backend/src/modules/queue/queue.module.ts
+++ apps/backend/src/modules/queue/queue.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { QueueConsumerService } from './queue-consumer.service';
 import { QueueService } from './queue.service';
 
 @Module({
     imports: [PrismaModule],
-    providers: [QueueService],
-    exports: [QueueService],
+    providers: [QueueService, QueueConsumerService],
+    exports: [QueueService, QueueConsumerService],
 })
 export class QueueModule { }
-
