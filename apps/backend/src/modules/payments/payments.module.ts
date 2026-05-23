e7164e20aac7c2284883a63012cdb66a9c6fe03e
Ji Hyung Geun <vndanwl@naver.com>
2026-05-23 18:50:20 +0900

    백엔드 크론 의존성 export 보강

--- apps/backend/src/modules/payments/payments.module.ts
+++ apps/backend/src/modules/payments/payments.module.ts
@@ -10,5 +10,6 @@ import { PaymentsService } from './payments.service';
     imports: [ConfigModule, PrismaModule, TossModule, QueueModule],
     controllers: [PaymentsController],
     providers: [PaymentsService],
+    exports: [PaymentsService],
 })
 export class PaymentsModule { }
